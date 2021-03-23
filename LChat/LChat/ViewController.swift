ae5dbfccceac9c51654e4ca5e693af5f7a1cd2eb
Egor Gurba <gurba21egor@gmail.com>
2021-03-23 11:24:18 +0200

    Add project

---
+++ LChat/LChat/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  LChat
+//
+//  Created by Егор on 23.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
